011ff813e7895979257a5ed1b6b46bd43511e1d8
Synchronizzed <supremeterminal@gmail.com>
2017-10-07 00:47:19 -0400

    setup build directory

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './index.js',
     output: {
         path: path.resolve(__dirname,''),
-        filename: 'transpiled.js'
+        filename: 'build/transpiled.js'
     },
     module: {
         loaders: [
