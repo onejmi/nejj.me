e02507fdc26dcf0a56ed4441f461f87823a631fc
Nayalash Mohammad <1nayalashm@gmail.com>
2020-01-15 21:13:53 -0500

    initial commit of nm-ver

--- README.md
+++
@@ -1,4 +0,0 @@
-# scarger.github.io
-Welcome to the source of my website!
-
-(In construction...)
--- legacy/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './src/App';
-
-//render everything, spout it onto #root div
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root')
-);
