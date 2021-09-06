cad4c1bdc62b8377cfa8f6cb21b90e234562d8bb
scarger <supremeterminal@gmail.com>
2021-09-06 18:04:28 -0400

    added disrupt-tracker

---
+++ projects/disrupt-tracker/precache-manifest.da947ec8d447638f02ba32c5be6dba15.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f346d5bd692627ee26a39e4766922b83",
+    "url": "/auth/success.html"
+  },
+  {
+    "revision": "abba89f8e373bbc170b4",
+    "url": "/css/chunk-vendors.896e5f23.css"
+  },
+  {
+    "revision": "0dfafb8c1e77b718f354",
+    "url": "/css/settings.9357cc44.css"
+  },
+  {
+    "revision": "e35a7cd7e3f1accc1830",
+    "url": "/css/stats.8ab79986.css"
+  },
+  {
+    "revision": "25c75fe8b71895101e6365948506e10b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "015c85fb354a3ea6c01c",
+    "url": "/js/app.fad9e9f2.js"
+  },
+  {
+    "revision": "abba89f8e373bbc170b4",
+    "url": "/js/chunk-vendors.ee4c6d73.js"
+  },
+  {
+    "revision": "9b5c1e4c1b4464ebb71a",
+    "url": "/js/login.178ffdcc.js"
+  },
+  {
+    "revision": "0dfafb8c1e77b718f354",
+    "url": "/js/settings.49061f5b.js"
+  },
+  {
+    "revision": "e35a7cd7e3f1accc1830",
+    "url": "/js/stats.7f8e151d.js"
+  },
+  {
+    "revision": "6fb79685f89dc8ece5264be574330af4",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
